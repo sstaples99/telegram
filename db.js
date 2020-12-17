b77cce8150f572520422383741c5f5eb1701ea20
Sean Stapleton <seanstapleton100@gmail.com>
2020-12-17 16:57:09 -0600

    updated db string

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const url = `mongodb://${process.env.db_user}:${process.env.db_pass}@ds011903.mlab.com:11903/telegram`;
+const url = `mongodb://${process.env.db_user}:${process.env.db_pass}@telegram-shard-00-00.s9vo5.mongodb.net:27017,telegram-shard-00-01.s9vo5.mongodb.net:27017,telegram-shard-00-02.s9vo5.mongodb.net:27017/telegram?ssl=true&replicaSet=atlas-5mzy9e-shard-0&authSource=admin&retryWrites=true&w=majority`;
 mongoose.connect(url);
 
 module.exports = mongoose.connection;
